bc68047157e8c5f9d1786044bb2599583b08625e
Agus Pujianto <53058544+masagus31@users.noreply.github.com>
2023-04-12 22:50:56 +0700

    Update sansekai.js

--- sansekai.js
+++ sansekai.js
@@ -3,9 +3,10 @@ const fs = require('fs')
 const util = require('util')
 const chalk = require('chalk')
 const { Configuration, OpenAIApi } = require("openai")
-let setting = require('dotenv').config();
 const apiKey = process.env.API_KEY_OPENAI;
 console.log(apiKey);
+
+let setting = require('dotenv').config();
 
 const BOT_NAME = process.env.BOT_NAME || "Lily Shania";
 const autoAI = process.env.autoAI || "True";
