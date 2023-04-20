7f20771db8b0dc88dcdcecae8dacbe16334a391a
Agus Pujianto <53058544+masagus31@users.noreply.github.com>
2023-04-21 04:21:12 +0700

    Update sansekai.js

--- sansekai.js
+++ sansekai.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const util = require('util')
 const chalk = require('chalk')
 const { Configuration, OpenAIApi } = require("openai")
-let setting = require ('process.env.API_KEY_OPENAI')
+let setting = require ('./accesser.json')
 const BOT_NAME = process.env.BOT_NAME ?? "Lily Shania";
 
 module.exports = sansekai = async (client, m, chatUpdate, store) => {
